000ec9defe2cd9cd8684f33845ac927b7e73ff3c
awaisabir <awaisabirqureshi@gmail.com>
2017-05-20 21:33:46 -0400

    working on search

--- src/components/search.js
+++ src/components/search.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 class Search extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      term: '' 
+    }
+  }
+
   render() {
     return(
       <div>
-        <input  onChange={this.onInputChange} />
+        {/* bind input value to the state */}
+        <input 
+          value = {this.state.term} 
+          onChange={event => this.setState({ term: event.target.value })} 
+        />
+        <div>{this.state.term}</div>
       </div>
     )
-  }
-
-  onInputChange(e) {
-    console.log(e.target.value)
   }
 }
 
